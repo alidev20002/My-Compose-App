da18e12a6d8752025c46ab24dcf735c859ebed7f
alidev20002 <alitigercr71381@gmail.com>
2023-07-20 11:20:29 -0700

    handle onConflict in movie dao

--- app/src/main/java/com/example/composeproject/data/local/db/daos/MovieDao.kt
+++ app/src/main/java/com/example/composeproject/data/local/db/daos/MovieDao.kt
@@ -2,6 +2,7 @@ package com.example.composeproject.data.local.db.daos
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.composeproject.data.local.db.entities.Movie
 import kotlinx.coroutines.flow.Flow
@@ -12,6 +13,7 @@ interface MovieDao {
     @Query("SELECT * FROM movie")
     fun getAllMovies(): Flow<List<Movie>>
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertMovie(movie: Movie)
+
 }
