d186b44e8ad0113c3969aeababff38bd09c7db8e
alidev20002 <alitigercr71381@gmail.com>
2023-07-20 11:21:11 -0700

    make movie type converter public

--- app/src/main/java/com/example/composeproject/data/local/db/converters/MovieConverter.kt
+++ app/src/main/java/com/example/composeproject/data/local/db/converters/MovieConverter.kt
@@ -5,12 +5,12 @@ import androidx.room.TypeConverter
 class MovieConverter {
 
     @TypeConverter
-    private fun stringToList(value: String): List<String> {
+    fun stringToList(value: String): List<String> {
         return value.split(",")
     }
 
     @TypeConverter
-    private fun listToString(values: List<String>): String {
+    fun listToString(values: List<String>): String {
         return values.joinToString(separator = ",")
     }
 
