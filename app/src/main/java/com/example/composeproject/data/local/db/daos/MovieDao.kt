945658c0e02f06d6816e11cd37c3e2fbc47606cd
alidev20002 <alitigercr71381@gmail.com>
2023-07-20 08:45:46 -0700

    creating movie dao (select and insert query)

---
+++ app/src/main/java/com/example/composeproject/data/local/db/daos/MovieDao.kt
@@ -0,0 +1,17 @@
+package com.example.composeproject.data.local.db.daos
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.composeproject.data.local.db.entities.Movie
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface MovieDao {
+
+    @Query("SELECT * FROM movie")
+    fun getAllMovies(): Flow<List<Movie>>
+
+    @Insert
+    suspend fun insertMovie(movie: Movie)
+}
