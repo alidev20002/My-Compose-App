57037b35b4b1b6c8890037ce94ca34d96c460c21
alidev20002 <alitigercr71381@gmail.com>
2023-07-20 08:26:36 -0700

    creating movie entity

--- app/src/main/java/com/example/composeproject/data/local/db/entities/Movie.kt
+++ app/src/main/java/com/example/composeproject/data/local/db/entities/Movie.kt
@@ -1,5 +1,17 @@
 package com.example.composeproject.data.local.db.entities
 
-data class Movie(
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
+@Entity(tableName = "movie")
+data class Movie(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val title: String,
+    val poster: String,
+    val year: String,
+    val country: String,
+    val imdb_rating: String,
+    val genres: String,
+    val images: String
 )
