b0e43dd2cb744355cf070203d092ef55ca847908
alidev20002 <alitigercr71381@gmail.com>
2023-07-20 16:32:35 -0700

    create fetch movies method to get flow of movies
    https://jira.ts.co.ir/browse/AND-6433

--- app/src/main/java/com/example/composeproject/data/repositories/MovieRepository.kt
+++ app/src/main/java/com/example/composeproject/data/repositories/MovieRepository.kt
@@ -1,18 +1,33 @@
 package com.example.composeproject.data.repositories
 
 import com.example.composeproject.data.local.db.MovieLocalDataSource
+import com.example.composeproject.data.local.db.entities.MovieEntity
+import com.example.composeproject.data.local.db.entities.toFullMovieModel
 import com.example.composeproject.data.network.MovieRemoteDataSource
 import com.example.composeproject.data.network.model.FullMovie
+import com.example.composeproject.data.network.model.toEntityModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
 class MovieRepository(
     private val movieLocalDataSource: MovieLocalDataSource,
     private val movieRemoteDataSource: MovieRemoteDataSource
 ) {
 
-    suspend fun fetchMovies(): List<FullMovie> {
+    suspend fun updateMovies() {
         val movies = movieRemoteDataSource.getAllMovies()
-        movies.forEach {
-            movieLocalDataSource.insertMovie(it)
+        movieLocalDataSource.insertMovies(
+            movies.map {
+                it.toEntityModel()
+            }
+        )
+    }
+
+    fun fetchMovies(): Flow<List<FullMovie>> {
+        return movieLocalDataSource.getAllMovies().map { movieEntities ->
+            movieEntities.map { movieEntity ->
+                movieEntity.toFullMovieModel()
+            }
         }
     }
 }
