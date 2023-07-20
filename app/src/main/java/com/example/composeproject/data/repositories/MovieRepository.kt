f918ae125fa6950611100582459ab1ec293c9005
alidev20002 <alitigercr71381@gmail.com>
2023-07-20 16:00:50 -0700

    implement fetch movies in movie repository
    https://jira.ts.co.ir/browse/AND-6433

--- app/src/main/java/com/example/composeproject/data/repositories/MovieRepository.kt
+++ app/src/main/java/com/example/composeproject/data/repositories/MovieRepository.kt
@@ -1,4 +1,18 @@
 package com.example.composeproject.data.repositories
 
-class MovieRepository {
+import com.example.composeproject.data.local.db.MovieLocalDataSource
+import com.example.composeproject.data.network.MovieRemoteDataSource
+import com.example.composeproject.data.network.model.FullMovie
+
+class MovieRepository(
+    private val movieLocalDataSource: MovieLocalDataSource,
+    private val movieRemoteDataSource: MovieRemoteDataSource
+) {
+
+    suspend fun fetchMovies(): List<FullMovie> {
+        val movies = movieRemoteDataSource.getAllMovies()
+        movies.forEach {
+            movieLocalDataSource.insertMovie(it)
+        }
+    }
 }
