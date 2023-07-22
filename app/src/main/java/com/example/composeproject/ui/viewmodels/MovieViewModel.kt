56bfebfe57ce3ed406f9326e26b33561e38ddc7b
alidev20002 <alitigercr71381@gmail.com>
2023-07-22 08:11:42 -0700

    implement unidirectional data flow in movie view model
    https://jira.ts.co.ir/browse/AND-6436

--- app/src/main/java/com/example/composeproject/ui/viewmodels/MovieViewModel.kt
+++ app/src/main/java/com/example/composeproject/ui/viewmodels/MovieViewModel.kt
@@ -1,5 +1,14 @@
 package com.example.composeproject.ui.viewmodels
 
-class MovieViewModel {
+import androidx.lifecycle.ViewModel
+import com.example.composeproject.data.network.model.FullMovie
+import com.example.composeproject.data.repositories.MovieRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.emptyFlow
 
+class MovieViewModel(private val movieRepository: MovieRepository): ViewModel() {
+    private val _allMovies: MutableStateFlow<List<FullMovie>> = MutableStateFlow(emptyList())
+    val allMovies: StateFlow<List<FullMovie>> = _allMovies.asStateFlow()
 }
