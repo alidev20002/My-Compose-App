da7ddbd0f3c7087ecb1ee623b02f0fc2b27f6a75
alidev20002 <alitigercr71381@gmail.com>
2023-07-22 08:27:37 -0700

    implement get movies method in movie view model
    https://jira.ts.co.ir/browse/AND-6436

--- app/src/main/java/com/example/composeproject/ui/viewmodels/MovieViewModel.kt
+++ app/src/main/java/com/example/composeproject/ui/viewmodels/MovieViewModel.kt
@@ -1,14 +1,25 @@
 package com.example.composeproject.ui.viewmodels
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.composeproject.data.network.model.FullMovie
 import com.example.composeproject.data.repositories.MovieRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.emptyFlow
+import kotlinx.coroutines.launch
 
 class MovieViewModel(private val movieRepository: MovieRepository): ViewModel() {
     private val _allMovies: MutableStateFlow<List<FullMovie>> = MutableStateFlow(emptyList())
     val allMovies: StateFlow<List<FullMovie>> = _allMovies.asStateFlow()
+
+    fun getAllMovies() {
+        viewModelScope.launch {
+            val movies = movieRepository.fetchMovies()
+            movies.collect {
+                _allMovies.value = it
+            }
+        }
+    }
+
 }
