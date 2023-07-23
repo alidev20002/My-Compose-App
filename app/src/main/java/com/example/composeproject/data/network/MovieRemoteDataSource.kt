ea1fbbe4b3768a742a9b800e1a5ae58cf1cb3ee8
alidev20002 <alitigercr71381@gmail.com>
2023-07-23 13:33:18 -0700

    fix offline mode
    https://jira.ts.co.ir/browse/AND-6436

--- app/src/main/java/com/example/composeproject/data/network/MovieRemoteDataSource.kt
+++ app/src/main/java/com/example/composeproject/data/network/MovieRemoteDataSource.kt
@@ -1,12 +1,19 @@
 package com.example.composeproject.data.network
 
+import android.util.Log
 import com.example.composeproject.data.network.api.ApiMovie
 import com.example.composeproject.data.network.model.FullMovie
 
 class MovieRemoteDataSource(private val apiMovie: ApiMovie) {
 
     suspend fun getAllMovies(): List<FullMovie> {
-        val movieData = apiMovie.getAllMovies(0, "")
-        return movieData.data
+        try {
+            val response = apiMovie.getAllMovies(0, "")
+            Log.i("alitest", "getAllMovies: $response")
+            return response.body()?.data ?: emptyList()
+        }catch (e: Exception) {
+            Log.e("message", e.message.toString())
+        }
+        return emptyList()
     }
 }
