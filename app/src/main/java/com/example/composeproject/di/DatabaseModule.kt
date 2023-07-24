92e0652fe4c6ee5fdedfb534c04a84a25e766c21
alidev20002 <alitigercr71381@gmail.com>
2023-07-24 09:38:32 -0700

    create movie database provider
    https://jira.ts.co.ir/browse/AND-6452

--- app/src/main/java/com/example/composeproject/di/DatabaseModule.kt
+++ app/src/main/java/com/example/composeproject/di/DatabaseModule.kt
@@ -1,11 +1,23 @@
 package com.example.composeproject.di
 
+import android.content.Context
+import com.example.composeproject.data.local.db.MovieDatabase
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideMovieDatabase(@ApplicationContext app: Context): MovieDatabase {
+        return MovieDatabase.getDatabase(app)
+    }
+
 
 }
