ce695dc3eaba465150420bd085277b5df46a8c9f
alidev20002 <alitigercr71381@gmail.com>
2023-07-24 14:28:50 -0700

    adjust lod ui with new architecture
    https://jira.ts.co.ir/browse/AND-6452

--- app/src/main/java/com/example/composeproject/MainActivity.kt
+++ app/src/main/java/com/example/composeproject/MainActivity.kt
@@ -1,39 +1,42 @@
 package com.example.composeproject
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
-import com.example.composeproject.data.local.db.MovieDatabase
-import com.example.composeproject.data.local.db.MovieLocalDataSource
-import com.example.composeproject.data.network.MovieRemoteDataSource
-import com.example.composeproject.data.network.api.ApiMovie
-import com.example.composeproject.data.repository.MovieRepository
-import com.example.composeproject.ui.viewmodels.MovieViewModelFactory
+import com.example.composeproject.ui.FullMoviePage
+import com.example.composeproject.ui.MoviePage
+import com.example.composeproject.ui.theme.ComposeProjectTheme
 import com.example.composeproject.viewmodel.CryptoViewModel
 import com.example.composeproject.viewmodel.MovieViewModel
 import com.example.composeproject.viewmodel.TaskViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -48,78 +51,40 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         // this is test area and will be removed soon
-
-        val db = MovieDatabase.getDatabase(this)
-        val movieDao = db.movieDao()
-        val movieLocalDataSource = MovieLocalDataSource(movieDao)
-        val movieRemoteDataSource = MovieRemoteDataSource(ApiMovie.getInstance())
-        val movieRepository = MovieRepository(movieLocalDataSource, movieRemoteDataSource)
-        val movieViewModel = ViewModelProvider(this, MovieViewModelFactory(movieRepository)).get(com.example.composeproject.ui.viewmodels.MovieViewModel::class.java)
-
-
-        val movies = movieViewModel.allMovies
-        movieViewModel.syncMovies()
-        Log.i(tag, "onCreate: Start")
-
-        lifecycleScope.launch {
-            movies.collect {
-                Log.i(tag, "onCreate: $it")
-            }
-        }
-
-        setContent {
-            val movieItems by movies.collectAsState(initial = emptyList())
-            LazyColumn(modifier = Modifier) {
-                itemsIndexed(movieItems, key = {index, _ -> index}) {_, item ->
-                    Row(modifier = Modifier.padding(5.dp), horizontalArrangement = Arrangement.SpaceBetween) {
-                        GlideImage(
-                            model = item.poster,
-                            contentDescription = "poster",
-                            modifier = Modifier.clip(CircleShape).size(50.dp),
-                            contentScale = ContentScale.Crop
-                        )
-                        Text(text = item.title)
-                        Text(text = item.year)
-                    }
-                }
-            }
-        }
-
         // #########################################
 
-//        setContent {
-//            var isLightTheme by rememberSaveable { mutableStateOf(true) }
-//            val navController = rememberNavController()
-//
-//            ComposeProjectTheme (darkTheme = !isLightTheme) {
-//
-//                Surface(modifier = Modifier.fillMaxSize(),
-//                    color = MaterialTheme.colors.background) {
-//
-//                    NavHost(navController = navController,
-//                        startDestination = "splash") {
-//
+        setContent {
+            val movieViewModel = viewModel<com.example.composeproject.ui.viewmodel.MovieViewModel>()
+            var isLightTheme by rememberSaveable { mutableStateOf(true) }
+            val navController = rememberNavController()
+
+            ComposeProjectTheme (darkTheme = !isLightTheme) {
+
+                Surface(modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background) {
+
+                    NavHost(navController = navController,
+                        startDestination = "movie") {
+
 //                        composable("splash") {
 //                            SplashScreen(navController)
 //                        }
-//                        composable("movie") {
-//                            MoviePage(movieViewModel,
-//                                isLightTheme,
-//                                navController,
-//                                onChangeTheme = {isLightTheme = !isLightTheme}
-//                            )
-//
-//                            movieViewModel.getMovieList()
-//                        }
-//                        composable("fullMovie") {
-//                            FullMoviePage(movieViewModel.fullMovieResponse,
-//                                isLightTheme,
-//                                navController) {
-//                                isLightTheme = !isLightTheme
-//                            }
-//
-//                            movieViewModel.getFullMovie()
-//                        }
+                        composable("movie") {
+                            MoviePage(movieViewModel,
+                                isLightTheme,
+                                navController,
+                                onChangeTheme = {isLightTheme = !isLightTheme}
+                            )
+
+                            movieViewModel.syncMovies()
+                        }
+                        composable("fullMovie") {
+                            FullMoviePage(movieViewModel.movieDetail!!,
+                                isLightTheme,
+                                navController) {
+                                isLightTheme = !isLightTheme
+                            }
+                        }
 //                        composable("crypto") {
 //                            CryptoPage(cryptoViewModel.cryptoStats)
 //                            cryptoViewModel.getCryptoStats()
@@ -127,10 +92,10 @@ class MainActivity : ComponentActivity() {
 //                        composable("task") {
 //                            TaskList(taskViewModel)
 //                        }
-//                    }
-//                }
-//            }
-//        }
+                    }
+                }
+            }
+        }
     }
 }
 
--- app/src/main/java/com/example/composeproject/ui/viewmodel/MovieViewModel.kt
+++ app/src/main/java/com/example/composeproject/ui/viewmodel/MovieViewModel.kt
@@ -2,6 +2,7 @@ package com.example.composeproject.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.composeproject.data.network.model.FullMovie
 import com.example.composeproject.data.repository.MovieRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -13,10 +14,17 @@ class MovieViewModel @Inject constructor(
 ): ViewModel() {
 
     val allMovies = movieRepository.getMovies()
+    private var _movieDetail: FullMovie? = null
+    val movieDetail: FullMovie?
+        get() = _movieDetail
 
     fun syncMovies() {
         viewModelScope.launch {
             movieRepository.syncMovies()
         }
+    }
+
+    fun updateMovieDetail(movie: FullMovie) {
+        _movieDetail = movie
     }
 }
