be99ef23aee5c9ae7e78b04c6f94402f6a1cefe4
alidev20002 <alitigercr71381@gmail.com>
2023-07-24 11:45:05 -0700

    create hilt movie view model
    https://jira.ts.co.ir/browse/AND-6452

---
+++ app/src/main/java/com/example/composeproject/ui/viewmodel/MovieViewModel.kt
@@ -0,0 +1,23 @@
+package com.example.composeproject.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.example.composeproject.data.repository.MovieRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MovieViewModel @Inject constructor(
+    private val movieRepository: MovieRepository
+): ViewModel() {
+
+    val allMovies = movieRepository.getMovies()
+
+    fun syncMovies() {
+        viewModelScope.launch {
+            movieRepository.syncMovies()
+        }
+    }
+}
