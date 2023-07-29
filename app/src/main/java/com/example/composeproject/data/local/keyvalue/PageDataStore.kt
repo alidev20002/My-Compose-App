b2dc9d18eb7c0ac37a716eb48caf1c931c8969d0
alidev20002 <alitigercr71381@gmail.com>
2023-07-29 11:38:31 -0700

    create page data store interface for page data source
    https://jira.ts.co.ir/browse/AND-6470

--- app/src/main/java/com/example/composeproject/data/local/keyvalue/PageDataStore.kt
+++ app/src/main/java/com/example/composeproject/data/local/keyvalue/PageDataStore.kt
@@ -5,15 +5,15 @@ import kotlinx.coroutines.flow.Flow
 
 class PageDataStore(
     private val context: Context
-) {
+): PageDataStoreInterface {
 
     private val baseKeyValue = KeyValueBase(context, FILE_NAME)
 
-    fun readPageNumber(): Flow<Int?> {
+    override fun readPageNumber(): Flow<Int?> {
         return baseKeyValue.readInt(PAGE_NUMBER_KEY)
     }
 
-    suspend fun writePageNumber(value: Int) {
+    override suspend fun writePageNumber(value: Int) {
         baseKeyValue.writeInt(PAGE_NUMBER_KEY, value)
     }
 
