5d66337b0eba139191223d9565957126bbefdb86
alidev20002 <alitigercr71381@gmail.com>
2023-08-05 15:06:41 -0700

    implement hilt for crypto worker
    https://jira.ts.co.ir/browse/AND-6502

--- app/src/main/java/com/example/composeproject/data/workers/CryptoWorker.kt
+++ app/src/main/java/com/example/composeproject/data/workers/CryptoWorker.kt
@@ -3,14 +3,21 @@ package com.example.composeproject.data.workers
 import android.annotation.SuppressLint
 import android.content.Context
 import android.util.Log
+import androidx.hilt.work.HiltWorker
 import androidx.work.CoroutineWorker
 import androidx.work.Data
 import androidx.work.WorkerParameters
 import com.example.composeproject.data.network.api.ApiCrypto
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 
-class CryptoWorker(ctx: Context, params: WorkerParameters) : CoroutineWorker(ctx, params) {
+@HiltWorker
+class CryptoWorker @AssistedInject constructor(
+    @Assisted ctx: Context,
+    @Assisted params: WorkerParameters
+) : CoroutineWorker(ctx, params) {
 
     @SuppressLint("RestrictedApi")
     override suspend fun doWork(): Result {
