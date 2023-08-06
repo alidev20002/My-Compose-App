d5ca3b3f94249caf5706bd30c7f5e2b79831595c
alidev20002 <alitigercr71381@gmail.com>
2023-08-06 13:04:34 -0700

    implement do work method of log worker
    https://jira.ts.co.ir/browse/AND-6502

--- app/src/main/java/com/example/composeproject/data/workers/LogWorker.kt
+++ app/src/main/java/com/example/composeproject/data/workers/LogWorker.kt
@@ -1,6 +1,7 @@
 package com.example.composeproject.data.workers
 
 import android.content.Context
+import android.util.Log
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 
@@ -8,6 +9,8 @@ class LogWorker(ctx: Context, params: WorkerParameters) : CoroutineWorker(ctx, p
 
     override suspend fun doWork(): Result {
         return try {
+            val cryptoData = inputData.getString("crypto")
+            Log.i("alitest", "log worker: i got the data :) -> $cryptoData")
             Result.success()
         }catch (e: Exception) {
             Result.failure()
