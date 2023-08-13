13281ec05686e21f85c68582b22a6895d4aa9618
alidev20002 <alitigercr71381@gmail.com>
2023-08-13 09:29:37 -0700

    remove unused imports

--- app/src/main/java/com/example/composeproject/data/local/db/MovieLocalDataSourceInterface.kt
+++ app/src/main/java/com/example/composeproject/data/local/db/MovieLocalDataSourceInterface.kt
@@ -2,7 +2,6 @@ package com.example.composeproject.data.local.db
 
 import androidx.paging.PagingSource
 import com.example.composeproject.data.local.db.entities.MovieEntity
-import kotlinx.coroutines.flow.Flow
 
 interface MovieLocalDataSourceInterface {
     fun getAllMovies(): PagingSource<Int, MovieEntity>
