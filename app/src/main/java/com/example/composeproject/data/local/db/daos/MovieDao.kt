97c504f6c2d21e887ae7904185d22bbc3912eb60
alidev20002 <alitigercr71381@gmail.com>
2023-08-13 09:26:46 -0700

    create search movie dao

--- app/src/main/java/com/example/composeproject/data/local/db/daos/MovieDao.kt
+++ app/src/main/java/com/example/composeproject/data/local/db/daos/MovieDao.kt
@@ -6,13 +6,15 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.composeproject.data.local.db.entities.MovieEntity
-import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface MovieDao {
 
     @Query("SELECT * FROM movie")
     fun getAllMovies(): PagingSource<Int, MovieEntity>
+
+    @Query("SELECT * FROM movie WHERE title LIKE '%' || :query || '%'")
+    fun searchMovie(query: String): PagingSource<Int, MovieEntity>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insertOrIgnoreMovie(movie: MovieEntity)
